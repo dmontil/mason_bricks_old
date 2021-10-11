66bc7ee3ba0827e45847695abba230aa631c55ed
monti <dmontil96@gmail.com>
2021-10-11 12:04:06 +0200

    fix underscore

--- bricks/feature/__brick__/{{#snakeCase}}{{name}}{{/snakeCase}}/insfraestructure/dto/{{#snakeCase}}{{name}}{{/snakeCase}}_response.dart
+++ bricks/feature/__brick__/{{#snakeCase}}{{name}}{{/snakeCase}}/insfraestructure/dto/{{#snakeCase}}{{name}}{{/snakeCase}}_response.dart
@@ -1,9 +1,9 @@
 
 import 'package:freezed_annotation/freezed_annotation.dart';
 
-part '{{#snakeCase}}{{name}}{{/snakeCase}}.freezed.dart';
+part '{{#snakeCase}}{{name}}{{/snakeCase}}_response.freezed.dart';
 {{#createJsonInDTO}} 
-part '{{#snakeCase}}{{name}}{{/snakeCase}}.g.dart'; 
+part '{{#snakeCase}}{{name}}{{/snakeCase}}_response.g.dart'; 
 {{/createJsonInDTO}} 
 
 @freezed
@@ -13,6 +13,6 @@ class {{#pascalCase}}{{name}}{{/pascalCase}}Response with _${{#pascalCase}}{{nam
   }) = _{{#pascalCase}}{{name}}{{/pascalCase}}Response;
 
 {{#createJsonInDTO}}
-  factory {{#pascalCase}}{{name}}{{/pascalCase}}.fromJson(Map<String, dynamic> json) => _${{#pascalCase}}{{name}}{{/pascalCase}}FromJson(json);
+  factory {{#pascalCase}}{{name}}{{/pascalCase}}Response.fromJson(Map<String, dynamic> json) => _${{#pascalCase}}{{name}}{{/pascalCase}}ResponseFromJson(json);
    
 {{/createJsonInDTO}} }
